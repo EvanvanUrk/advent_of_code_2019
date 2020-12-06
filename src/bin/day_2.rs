dcf29046a88c7c6593d336bead749ad8c9d65eb3
Evan van Urk <evanvurk@gmail.com>
2020-12-06 22:10:47 +0100

    Add day_2

---
+++ src/bin/day_2.rs
@@ -0,0 +1,72 @@
+extern crate advent_of_code_2019;
+
+use self::advent_of_code_2019::*;
+use self::advent_of_code_2019::int_code::*;
+use itertools::Itertools;
+
+fn main() {
+    let input = get_input("day_2.txt");
+
+    let code: Vec<i32> = input
+        .split(',')
+        .map(|op| op.parse().unwrap())
+        .collect();
+
+    let mut test_code = code.clone();
+    test_code[1] = 12;
+    test_code[2] = 2;
+
+    let mut computer = IntCodeComputer::new(test_code);
+    computer.run();
+
+    println!("{}", computer.get(0));
+
+    let pairs = (0..99).cartesian_product(0..99);
+    let mut answer = 0;
+    for pair in pairs {
+        let mut try_code = code.clone();
+        try_code[1] = pair.0;
+        try_code[2] = pair.1;
+
+        let mut computer = IntCodeComputer::new(try_code);
+        computer.run();
+
+        if computer.get(0) == 19690720 {
+            answer = computer.get(1) * 100 + computer.get(2);
+            break;
+        }
+    }
+
+    println!("{}", answer);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        let mut computer = IntCodeComputer::new(vec![1, 0, 0, 0, 99]);
+        computer.run();
+        assert_eq!(2, computer.get(0));
+    }
+
+    #[test]
+    fn test_multiply() {
+        let mut computer = IntCodeComputer::new(vec![2, 3, 0, 3, 99]);
+        computer.run();
+        assert_eq!(6, computer.get(3));
+    }
+
+    #[test]
+    fn test_intcode() {
+        let mut computer = IntCodeComputer::new(vec![2, 4, 4, 5, 99, 0]);
+        computer.run();
+        assert_eq!(9801, computer.get(5));
+
+        let mut computer = IntCodeComputer::new(vec![1, 1, 1, 4, 99, 5, 6, 0, 99]);
+        computer.run();
+        assert_eq!(30, computer.get(0));
+        assert_eq!(2, computer.get(4));
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -19,3 +19,65 @@ pub fn get_input(file_path: &str) -> String {
 
     input
 }
+
+pub mod int_code {
+    pub struct IntCodeComputer {
+        memory: Vec<i32>,
+        pointer: usize,
+        running: bool,
+    }
+
+    impl IntCodeComputer {
+        pub fn new(code: Vec<i32>) -> IntCodeComputer {
+            IntCodeComputer {
+                memory: code,
+                pointer: 0,
+                running: false,
+            }
+        }
+
+        pub fn run(&mut self) {
+            self.running = true;
+            while self.running {
+                self.pointer += self.do_current_op();
+            }
+        }
+
+        pub fn get(&self, offset: usize) -> i32 {
+            self.memory[self.pointer + offset]
+        }
+
+        fn do_current_op(&mut self) -> usize {
+            match self.memory[self.pointer] {
+                1 => self.op_1(
+                    self.get(1) as usize,
+                    self.get(2) as usize,
+                    self.get(3) as usize
+                ),
+                2 => self.op_2(
+                    self.get(1) as usize,
+                    self.get(2) as usize,
+                    self.get(3) as usize
+                ),
+                99 => self.op_99(),
+                op => panic!("Unknown opcode: {}", op),
+            }
+        }
+
+        fn op_1(&mut self, a: usize, b: usize, store: usize) -> usize {
+            self.memory[store] = self.memory[a] + self.memory[b];
+            4
+        }
+
+        fn op_2(&mut self, a: usize, b: usize, store: usize) -> usize {
+            self.memory[store] = self.memory[a] * self.memory[b];
+            4
+        }
+
+        fn op_99(&mut self) -> usize {
+            self.running = false;
+            self.pointer = 0;
+            0
+        }
+    }
+}
